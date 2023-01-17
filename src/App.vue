8118897fb3d19e9073df3b64ae3f192af732cddd
vivientje97 <122792079+vivientje97@users.noreply.github.com>
2023-01-17 13:58:21 +0100

    1 binaries uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <vue-list-picker :left-items="leftItems" :right-items="rightItems" />
+</template>
 
 <script>
 import VueListPicker from './components/VueListPicker.vue';
@@ -8,6 +10,29 @@ export default {
   components: {
     VueListPicker,
   },
+  data() {
+      const example1 = {
+        key: 1,
+        content: "Item 1",
+      };
+      const example2 = {
+        key: 2,
+        content: "Item 2",
+      };
+      const example3 = {
+        key: 3,
+        content: "Item 3",
+      };
+      const example4 = {
+        key: 4,
+        content: "Item 4",
+      };
+
+      const leftItems = [example1, example2];
+      const rightItems = [example3, example4];
+
+      return { leftItems, rightItems };
+    },
 };
 </script>
 
